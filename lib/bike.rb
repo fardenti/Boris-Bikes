7b189601af592f05b0832bae46c3fdc36b2e50c6
Student <student@Makerss-MacBook-Pro.local>
2019-07-24 15:21:50 +0100

    Mario Exercise9

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,10 +1,4 @@
 class Bike
-<<<<<<< HEAD
   def working?
   end
 end
-=======
-    def working?
-    end
-end
->>>>>>> 54d23ae21cfdc3c602fe24b4f4f8eb918157e1f1
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,9 +1,4 @@
 require 'bike'
 describe Bike do
-<<<<<<< HEAD
     it {expect(subject).to respond_to(:working?)}
 end
-=======
-    it {expect(subject).to respond_to(:working?)}
-end
->>>>>>> 54d23ae21cfdc3c602fe24b4f4f8eb918157e1f1
